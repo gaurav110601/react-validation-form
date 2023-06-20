96d1818bce178e3882ddfdaf2577201dbe071cd0
GAURAV SHARMA <gaurav110601@gmail.com>
2023-06-20 16:16:46 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/components/Home.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/components/Home.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import validator from 'validator';
+
+function Home(props) {
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [mobile,setMobile]=useState('');
+    const [city,setCity]=useState('');
+    const [college,setCollege]=useState('');
+    const [checkSubmit,setChecksubmit]=useState(false);
+    // const [errormsg,setErrormsg]=useState('');
+
+    function validateForm (){
+      if(!validator.isEmail(email))
+      {
+        alert('Email is Invalid');
+        return;
+      }
+      if(!validator.isMobilePhone(mobile))
+      {
+        alert('Mobile number is Invalid');
+        return;
+      }
+      for(let i=0;i<mobile.length;i++)
+      {
+        if(mobile[i]-'0'<0 || mobile[i]-'0'>9)
+        {
+          alert('Mobile number is Invalid');
+          return;
+        }
+      }
+        setChecksubmit(true);
+    }
+  return (
+    <>
+    {!checkSubmit && (
+      <>
+      <div>
+      <br />  <h2><u>Register here</u></h2>
+      </div>
+      <form onSubmit={validateForm} style={{marginTop:"30px"}}>
+      <input type="text" 
+      placeholder='name' 
+      value={name}
+      onChange={(ev)=>{setName(ev.target.value)}}
+      required/>
+      <input type="email" 
+      placeholder='email' 
+      value={email}
+      onChange={(ev)=>{setEmail(ev.target.value)}}
+      required/>
+      <input type="text"
+      placeholder='Mobile No.' 
+      value={mobile} 
+      onChange={(ev)=>{setMobile(ev.target.value)}}
+      required/>
+      <input type="text" 
+      placeholder='City'
+      value={city}
+      onChange={(ev)=>{setCity(ev.target.value)}}
+      required/>
+      <input type="text" 
+      placeholder='College'
+      value={college}
+      onChange={(ev)=>{setCollege(ev.target.value)}}
+      required/>
+      
+      <button>Submit</button>
+  </form></>
+    )}
+    {checkSubmit && (
+      <>
+      <div><br />
+        <h2>Thanks for Registering</h2>
+      </div>
+      <div className="display">
+        <center><table>
+        <tr>
+            <td><b>Name : </b></td> 
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td><b>Email : </b></td>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <td><b>Mobile : </b></td>
+            <td>{mobile}</td>
+          </tr>
+          <tr>
+            <td><b>City : </b></td>
+            <td>{city}</td>
+          </tr>
+          <tr>
+            <td><b>College : </b></td>
+            <td>{college}</td>
+          </tr>
+        </table></center>
+      </div>
+      </>
+    )}
+    </>
+  )
+}
+
+export default Home;
